aa4cdc47f4ce814d172c5bfab36265572f3cd738
ivangacha <ivangacha@gmail.com>
2024-09-12 21:13:24 -0500

    Se modifica el metodo agregarProducto para controlar que no se ingresen valores negativos en la precio del producto, se agrega el metodo agregarVenta para ingresar las ventas por vendedor, se agrega el metodo generarArchivoRecaudacionPorVendedor para generar archivo CSV de las ventas de vendedores ordenados por recaudacion del mayor al menor y se crea el metodo borrarArchivosCSV para que limpie los archivos al iniciar el programa

--- src/proyectoentrega_1/ProyectoEntrega_1.java
+++ src/proyectoentrega_1/ProyectoEntrega_1.java
@@ -4,11 +4,14 @@
  */
 package proyectoentrega_1;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -17,6 +20,9 @@ import java.util.ArrayList;
 public class ProyectoEntrega_1 {
     
     public static void main(String[] args) {
+        
+        //Llamada al metodo borrarArchivosCSV para eliminar los archivos existentes
+        borrarArchivosCSV();
         
         //Se crea la variable scanner para obtener los datos ingresados por consola
         Scanner scanner = new Scanner(System.in);
@@ -29,7 +35,30 @@ public class ProyectoEntrega_1 {
         
     }
     
-    //Menu
+    //Metodo para eliminar los archivos que esten creados
+    public static void borrarArchivosCSV(){
+        
+        //Nombres de los archivos a eliminar
+        String [] archivosCSV = {"vendedores.csv", "productos.csv", "recaudacion_vendedores.csv"};
+        
+        //Ciclo para intentar borrar cada uno de los archivos
+        for (String nombreArchivo : archivosCSV){
+            File archivo = new File(nombreArchivo);
+            
+            if (archivo.exists()){
+                if (archivo.delete()){
+                    System.out.println("El archivo: " + nombreArchivo + " fue borrado exitosamente.");
+                } else{
+                    System.out.println("No se pudo eliminar el archivo " +nombreArchivo);
+                }
+            } else{
+                System.out.println("El archivo " + nombreArchivo + " no existe.");
+            }
+        }
+        
+    }
+    
+    //Menu **********************************************
     
     //Metodo para encapsular el menu
     public static void mostarMenu(Scanner scanner){
@@ -40,10 +69,13 @@ public class ProyectoEntrega_1 {
         //Creacion de una lista para ingresar la informacion de los productos
         List<Producto> listaProductos = new ArrayList<>();
         
+        //Creacion de una lista para ingresar la informacion de las ventas
+        List<Venta> listaVentas = new ArrayList<>();
+        
         int opcion = 0;
         
         //Ciclo para las diferentes opciones del menu, el ciclo se repite hasta el usuario elija la opcion de salida
-        while (opcion != 7){
+        while (opcion != 9){
             System.out.println("\n---- MENU ----");
             System.out.println("1. Agregar vendedor");
             System.out.println("2. Listar vendedores");
@@ -51,7 +83,9 @@ public class ProyectoEntrega_1 {
             System.out.println("4. Agregar producto");
             System.out.println("5. Listar productos");
             System.out.println("6. Generar archivo CSV de productos");
-            System.out.println("7. Salir");
+            System.out.println("7. Agregar venta");
+            System.out.println("8. Generar archivo CSV de ventas, ordenados de mayor a menor");
+            System.out.println("9. Salir");
             System.out.println("Seleccione una opcion: ");
             
             //Leer la opcion ingresada por el usuario
@@ -110,6 +144,22 @@ public class ProyectoEntrega_1 {
                     pausaYLimpiarConsola();
                     break;
                 case 7:
+                    System.out.println("Has escogido la opcion 7.");
+                    
+                    //Llamada al metodo agregarVenta para ingresar la informacion de las ventas por vendedor
+                    agregarVenta(listaVentas, listaVendedores, listaProductos, scanner);
+                    
+                    pausaYLimpiarConsola();
+                    break;
+                case 8:
+                    System.out.println("Has escogido la opcion 8.");
+                    
+                    //Llamada al metodo generarArchivoRecaudacionPorVendedor para generar el archivo CSV con la información de las ventas por vendedor
+                    generarArchivoRecaudacionPorVendedor(listaVentas, "recaudacion_vendedores.csv");
+                    
+                    pausaYLimpiarConsola();
+                    break;
+                case 9:
                     System.out.println("Saliendo del menu...");
                     break;
                 default:
@@ -134,7 +184,7 @@ public class ProyectoEntrega_1 {
         }
     }
     
-    //Metodos para vendedores
+    //Metodos para vendedores **********************************************
     
     //Metodo agregarVendedor que permite ingresar la informacion de cada vendedor
     public static void agregarVendedor(List<Vendedor> listaVendedores, Scanner scanner){
@@ -244,7 +294,7 @@ public class ProyectoEntrega_1 {
         }
     }
     
-    //Metodos para productos
+    //Metodos para productos **********************************************
     
     //Metodo agregarProducto que permite ingresar la informacion de cada producto
     public static void agregarProducto(List<Producto> listaProductos, Scanner scanner){
@@ -257,8 +307,9 @@ public class ProyectoEntrega_1 {
 
             System.out.println("Ingrese la informacion del producto:");
             
-            //Variable 
+            //Variable que controla la condicion del while
             boolean validador = true;
+            
             //Variable id que almacenara el ID del producto a ingresar
             int id = 0;
             
@@ -268,6 +319,8 @@ public class ProyectoEntrega_1 {
                 //Se pide por consola ingresar el ID y se almacena en la variable id
                 System.out.println("Id: ");
                 id = scanner.nextInt();
+                
+                //Consumir el salto de línea pendiente después de nextInt
                 scanner.nextLine();
                 
                 //Variable para saber si el ID ya existe
@@ -292,12 +345,31 @@ public class ProyectoEntrega_1 {
             //Se pide por consola ingresar el nombre del producto y se almacena en la variable nombreProducto
             System.out.println("Nombre del producto: ");
             String nombreProducto = scanner.nextLine();
-
-            //Se pide por consola ingresar el precio de la unidad y se almacena en la variable precioPorUnidad
-            System.out.println("Precio por unidad: ");
-            Double precioPorUnidad = scanner.nextDouble();
-
-            scanner.nextLine();
+            
+            //Variable que controla la condicion del while
+            bandera = true;
+            
+            //Variable id que almacenara el ID del producto a ingresar
+            Double precioPorUnidad = 0.0;
+            
+            //Ciclo que controla que no se ingrese un valor de producto negativo
+            while (bandera){
+                
+                //Se pide por consola ingresar el precio de la unidad y se almacena en la variable precioPorUnidad
+                System.out.println("Precio por unidad: ");
+                precioPorUnidad = scanner.nextDouble();
+            
+                //Consumir el salto de línea pendiente después de nextDouble
+                scanner.nextLine();
+                
+                //Condicional que verifica que el precio de la unidad no sea negativo
+                if (precioPorUnidad < 0){
+                    System.out.println("El precio por unidad no puede ser negativo");
+                }else{
+                    bandera = false;
+                }
+                
+            }
 
             //Se crea un objeto de tipo producto
             Producto producto = new Producto(id, nombreProducto, precioPorUnidad);
@@ -346,6 +418,122 @@ public class ProyectoEntrega_1 {
             System.out.println("Archivo CSV de productos generado exitosamente.");
         } catch (IOException e) {
             System.out.println("Error al generar el archivo CSV de productos: " + e.getMessage());
+        }
+    }
+    
+    //Metodos para ventas **********************************************
+    
+    //Metodo agregarProducto que permite ingresar la informacion de cada producto
+    public static void agregarVenta(List<Venta> listaVentas, List<Vendedor> listaVendedores, List<Producto> listaProductos, Scanner scanner){
+        
+        //Variable que controla la condicion del while
+        boolean bandera = true;
+
+        //Ciclo que controla el registro de las ventas
+        while(bandera){
+            
+            System.out.println("Ingrese la informacion de la venta:");
+            
+            //Se declara la variable de tipo Vendedor y se inicializa con el valor null
+            Vendedor vendedorEncontrado = null;
+            
+            //Ciclo que controla que no se ingrese un numero de cedula de un vendedor no existente
+            while (vendedorEncontrado == null){
+                
+                //Se pide por consola ingresar la cedula del vendedor
+                System.out.println("Ingrese el numero de cedula del vendedor: ");
+                String numeroDocumento = scanner.nextLine();
+                
+                //Ciclo for que busca la cedula ingresada en la listaVendedores
+                for (Vendedor v : listaVendedores){
+                    //Condicional que compara el numero de cedula con el almacenado en la lista
+                    if (v.getNumeroDocumento().equals(numeroDocumento)){
+                        //Si la cedula existe se asigna al objeto vendedorEncontrado
+                        vendedorEncontrado = v;
+                        break;
+                    }
+                }
+                //Si el numero de cedula no existe, se solicita nuevamente ingresar la informacion
+                if (vendedorEncontrado == null){
+                    System.out.println("Error: No existe un vendedor con ese numero de documento, por favor intentelo de nuevo.");
+                }
+            }
+            
+            //Se declara la variable de tipo Producto y se inicializa con el valor null
+            Producto productoEncontrado = null;
+            
+            //Ciclo que controla que no se ingrese un ID de un producto no existente
+            while (productoEncontrado == null){
+                
+                //Se pide ingresar el ID del producto
+                System.out.println("Ingrese el ID del producto: ");
+                int id = scanner.nextInt();
+                
+                //Consumir el salto de línea pendiente después de nextInt
+                scanner.nextLine();
+                
+                //Ciclo for que busca el ID ingresado en la listaproductos
+                for (Producto p : listaProductos){
+                    //Condicional que compara el numero de ID con el almacenado en la lista
+                    if (p.getId() == id){
+                        //Si el ID existe se asigna al objeto productoEncontrado
+                        productoEncontrado = p;
+                        break;
+                    }
+                }
+                //Si el ID no existe, se solicita nuevamente ingresar la informacion
+                if (productoEncontrado == null){
+                    System.out.println("Error: No existe un producto con ese numero de ID, por favor intentelo de nuevo.");
+                }
+            }
+            
+            //Se pide por consola ingresar la cantidad vendida del producto y se almacena en la variable cantidadProducto
+            System.out.println("Ingrese la cantidad vendida: ");
+            int cantidadProducto = scanner.nextInt();
+            
+            //Consumir el salto de línea pendiente después de nextInt
+            scanner.nextLine();
+            
+            //Se crea un objeto de tipo venta
+            Venta venta = new Venta(vendedorEncontrado, productoEncontrado, cantidadProducto);
+
+            //El objeto creado se agrega a la lista de ventas
+            listaVentas.add(venta);
+
+            //Se pregunta al usuario si desea ingresar otro vendedor, de lo contrario finaliza el ciclo
+            System.out.println("Desea ingresar informacion de otro venta (si/no): ");
+            String respuesta = scanner.nextLine();
+            bandera = respuesta.equalsIgnoreCase("si");
+            
+        }
+        
+    }
+    
+    //Metodo generarArchivoRecaudacionPorVendedor que permite calcular la cantidad de dinero recaudado por vendedor y ordenarlos de menos a mayor y genera el archivo CSV correspondiente
+    public static void generarArchivoRecaudacionPorVendedor(List<Venta> listaVentas, String nombreArchivo) {
+        
+        // Calcular la cantidad de dinero recaudado por cada vendedor
+        Map<Vendedor, Double> recaudacionPorVendedor = new HashMap<>();
+        for (Venta venta : listaVentas) {
+            Vendedor vendedor = venta.getVendedor();
+            double totalVenta = venta.getProducto().getPrecioPorUnidad() * venta.getCantidadProducto();
+            recaudacionPorVendedor.put(vendedor, recaudacionPorVendedor.getOrDefault(vendedor, 0.0) + totalVenta);
+        }
+
+        // Ordenar los vendedores por cantidad de dinero recaudado, de mayor a menor
+        List<Map.Entry<Vendedor, Double>> listaRecaudacion = new ArrayList<>(recaudacionPorVendedor.entrySet());
+        listaRecaudacion.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+
+        // Crear el archivo CSV
+        try (FileWriter writer = new FileWriter(nombreArchivo)) {
+            for (Map.Entry<Vendedor, Double> entry : listaRecaudacion) {
+                Vendedor vendedor = entry.getKey();
+                double recaudacion = entry.getValue();
+                writer.write(vendedor.getNombres()+ ";" +vendedor.getApellidos()+ ";" + recaudacion + "\n");
+            }
+            System.out.println("Archivo generado exitosamente: " + nombreArchivo);
+        } catch (IOException e) {
+            System.err.println("Error al generar el archivo: " + e.getMessage());
         }
     }
     
